540ce49d80e66cb851f774f3aabf871e55f5c423
Artem <69482749+ArtemkaOffical@users.noreply.github.com>
2020-09-15 15:40:40 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  try{
+  var CommandName="";
+  for(j=0;j<members.length;j++)
+{
+  if(typeof(members[j])==typeof('string'))
+  for(i=0;i<members[j].length;i++)
+  {
+    members[j] = members[j].trim();
+    if(i==0)
+    {
+      
+      CommandName +=members[j][i];
+      
+    }
+  }
+  CommandName=CommandName.split("").sort().join("").toUpperCase();
 };
+return CommandName.split("").sort().join('');;
+  }catch(ex){}
+  return false;
+};
+ 
