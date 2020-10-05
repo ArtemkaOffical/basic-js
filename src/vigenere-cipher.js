048adb96f7d07f454fa5664a695567d0e3c6ed16
Artem <69482749+ArtemkaOffical@users.noreply.github.com>
2020-10-05 13:59:25 +0000

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(bool) {
+    if (bool === true || bool === undefined) this.reverse = true
+    else this.reverse = false
+  }
+  calculateOutput(message, key, method) {
+    if (!message || !key) throw new Error();
+    let output = '';
+    let keyLetterIterator = 0;
+    for (let i = 0; i < message.length; i++) 
+    {
+      const stringLetter = message[i].toUpperCase();
+      if (stringLetter >= 'A' && stringLetter <= 'Z')
+      {
+        const keyLetter = i < key.length ? key[keyLetterIterator].toUpperCase() : key[keyLetterIterator % key.length].toUpperCase();
+        keyLetterIterator++;
+        if(method =='encrypt')
+        {
+          output += String.fromCharCode((stringLetter.charCodeAt(0) + keyLetter.charCodeAt(0)) % 26 + 65) 
+        }
+        else{output += String.fromCharCode((26 + stringLetter.charCodeAt(0) - keyLetter.charCodeAt(0)) % 26 + 65)}
+      }
+      else output += message[i]
+    }
+    return this.reverse ? output : output.split('').reverse().join('')
+  }
+  encrypt(message, key) {
+    return this.calculateOutput(message, key, 'encrypt')
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(encryptedMessage, key) {
+    return this.calculateOutput(encryptedMessage, key, 'decrypt')
   }
 }
 
